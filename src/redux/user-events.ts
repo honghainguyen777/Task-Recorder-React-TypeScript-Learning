94811cd1b324a66317ad79e22e4e40771300a66f
honghainguyen777 <n.h.hai2015@gmail.com>
2021-04-02 11:17:35 +0200

    loading events - p1

--- src/redux/user-events.ts
+++ src/redux/user-events.ts
@@ -1,4 +1,6 @@
-import { AnyAction } from 'redux';
+import { Action, AnyAction } from 'redux';
+import { ThunkAction } from 'redux-thunk';
+import { RootState } from './store';
 
 interface UserEvent {
   id: number;
@@ -7,21 +9,75 @@ interface UserEvent {
   dateEnd: string;
 }
 
-interface UserEventState {
+interface UserEventsState {
   byIds: Record<UserEvent['id'], UserEvent>;
   allIds: UserEvent['id'][]; // normalizing data in redux term
 }
 
-const initialState: UserEventState = {
+const initialState: UserEventsState = {
   byIds: {},
   allIds: [],
 };
 
+const LOAD_REQUEST = 'userEvents/load_request';
+
+interface LoadRequestAction extends Action<typeof LOAD_REQUEST> {}
+
+const LOAD_SUCCESSS = 'userEvents/load_success';
+
+interface LoadSuccessAction extends Action<typeof LOAD_SUCCESSS> {
+  payload: {
+    events: UserEvent[];
+  };
+}
+
+const LOAD_FAILURE = 'userEvents/load_failure';
+
+interface LoadFailureAction extends Action<typeof LOAD_FAILURE> {
+  error: string;
+}
+
+export const loadUserEvents = (): ThunkAction<
+  void,
+  RootState,
+  undefined,
+  LoadRequestAction | LoadSuccessAction | LoadFailureAction
+> => async (dispatch, getState) => {
+  dispatch({
+    type: LOAD_REQUEST,
+  });
+
+  try {
+    const response = await fetch('http://localhost:3001/events');
+    const events: UserEvent[] = await response.json();
+
+    dispatch({
+      type: LOAD_SUCCESSS,
+      payload: { events },
+    });
+  } catch (e) {
+    dispatch({
+      type: LOAD_FAILURE,
+      error: 'Failed to load events!',
+    });
+  }
+};
+
 const userEventsReducer = (
-  state: UserEventState = initialState,
-  action: AnyAction
+  state: UserEventsState = initialState,
+  action: LoadSuccessAction
 ) => {
   switch (action.type) {
+    case LOAD_SUCCESSS:
+      const { events } = action.payload;
+      return {
+        ...state,
+        allIds: events.map(({ id }) => id),
+        byIds: events.reduce<UserEventsState['byIds']>((byIds, event) => {
+          byIds[event.id] = event;
+          return byIds;
+        }, {}),
+      };
     default:
       return state;
   }
